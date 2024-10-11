d8cff625cae0d3278f15d4b4920fd1432c85a6ed
laiwenjun <laiwenjun5789@163.com>
2024-10-11 16:03:22 +0800

    适配codewave依赖库，org.codehaus.jackson:jackson-mapper-asl版本变更:1.9.11-->1.8.7,

--- ureport2-console/src/main/java/com/bstek/ureport/console/WriteJsonServletAction.java
+++ ureport2-console/src/main/java/com/bstek/ureport/console/WriteJsonServletAction.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2017 Bstek
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -36,7 +36,9 @@ public abstract class WriteJsonServletAction extends BaseServletAction{
 		resp.setContentType("text/json");
 		resp.setCharacterEncoding("UTF-8");
 		ObjectMapper mapper=new ObjectMapper();
-		mapper.setSerializationInclusion(Inclusion.NON_NULL);
+		// 兼容org.codehaus.jackson:jackson-mapper-asl:1.8.7
+		mapper.getSerializationConfig().setSerializationInclusion(Inclusion.NON_NULL);
+//		mapper.setSerializationInclusion(Inclusion.NON_NULL);
 		mapper.configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS,false);
 		mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
 		OutputStream out = resp.getOutputStream();
